14b7e3b1f0d21f88c9b74ae9ea7376dc5ca57c90
Eduardo Marchena <emarchen@everis.com>
2018-10-22 15:35:55 -0500

    esta listo la primera parte hasta la reproduccion de videos

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -2,7 +2,10 @@ import React , {Component} from  'react';
 import HomeLayout from '../components/home-layout';
 import Related from '../components/related';
 import Categories from '../../categories/components/categories';
-
+import ModalContainer from '../../widgets/containers/modal';
+import Modal from '../../widgets/components/modal';
+import VideoPlayer from '../../player/containers/video-player';
+import HandleError from '../../error/containers/handle-error';
 class Home extends Component {
 	state = {
 		modalVisible : false,
@@ -22,13 +25,29 @@ class Home extends Component {
 
 	render (){
 		return (
-			<HomeLayout>
-				<Related/>
-				<Categories
-					categories={this.props.data.categories}
-					handleOpenModal={this.handleOpenModal}
-				/>
-			</HomeLayout>
+			<HandleError>
+				<HomeLayout>
+					<Related/>
+					<Categories
+						categories={this.props.data.categories}
+						handleOpenModal={this.handleOpenModal}
+					/>
+					{
+			            this.state.modalVisible &&
+			            <ModalContainer>
+			              <Modal
+			                handleClick={this.handleCloseModal}
+			              >
+			                <VideoPlayer
+			                  autoplay
+			                  src={this.state.media.src}
+			                  title={this.state.media.title}
+			                />
+			              </Modal>
+			            </ModalContainer>
+			          }
+				</HomeLayout>
+			  </HandleError>
 			);
 	}
 }
